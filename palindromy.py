bced30cbb6fab0791dff32b3fcf7bf4fb830b28d
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-06-01 20:11:19 +0200

    Add documentation

--- palindromy.py
+++ palindromy.py
@@ -3,6 +3,12 @@ words_items = [
     "potop"
 ]
 def palindrome(items):  
+    """
+    Prints result when words from list are palindromes
+    Arguments:
+    items from words_items
+    """
     return items == items [::-1]
 for item in words_items:
     print(palindrome(item))
+
