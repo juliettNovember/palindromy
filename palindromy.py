7251dd9d4d308e7b26a1d71ff960b2a23487a5f2
Justyna Nowakowska <nowakowska.just@gmail.com>
2020-06-01 21:31:37 +0200

    Making correction

--- palindromy.py
+++ palindromy.py
@@ -1,14 +1,14 @@
+def palindrome(sentence):
+    """
+    Return True if sentence is palindrom, otherwise False
+    """
+    return sentence == sentence[::-1]
+
 words_items = [
     "kajak",
-    "potop"
+    "potop",
 ]
-def palindrome(items):  
-    """
-    Prints result when words from list are palindromes
-    Arguments:
-    items from words_items
-    """
-    return items == items [::-1]
+
 for item in words_items:
     print(palindrome(item))
 
